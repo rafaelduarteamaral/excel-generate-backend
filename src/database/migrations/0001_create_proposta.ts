df7fbf121d7c4af21bba5605376d2e115f29089d
rafael duarte amaral <rafaelduarteamaral@pop-os.localdomain>
2022-01-26 14:18:43 -0300

    subindo formulario

---
+++ src/database/migrations/0001_create_proposta.ts
@@ -0,0 +1,28 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('proposta', (table: Knex.TableBuilder) => {
+    table.increments('id').primary();
+    table.string('cliente').nullable();
+    table.string('anoVeiculo').nullable();
+    table.string('origemVeiculo').nullable();
+    table.string('destinoVeiculo').nullable();
+    table.string('telefoneCelular').nullable();
+    table.string('dddCelular').nullable();
+    table.string('coletaVeiculo').nullable();
+    table.string('enderecoColetaVeiculo').nullable();
+    table.string('enderecoEntregaVeiculo').nullable();
+    table.string('obeservacao').nullable();
+    table.string('valorTotalVeiculo').nullable();
+    table.string('orcamentoServico').nullable();
+    table.string('coleta').nullable();
+    table.string('entrega').nullable();
+    table.string('obeservacao').nullable();
+    table.string('prazoEntrega').nullable();
+    table.timestamp('timestamp').defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  await knex.schema.dropTable('proposta');
+}
